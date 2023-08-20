598bae524eb939960d20c5b78b86f4b09cb53076
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-08-20 00:57:03 -0400

    Todos los años divisibles por 100 pero no por 400 NO son años bisiestos

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -3,6 +3,10 @@ function bisiesto(year) {
   if(year%400===0){
     return true
     }
+    
+  if(year%100===0){
+      return false
+    }
 }
 
 export default bisiesto;
--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -4,4 +4,8 @@ describe("Saber si es un Año Bisiesto", () => {
   it("Todos los años divisibles por  400 SON años bisiestos", () => {
     expect(bisiesto(2000)).toEqual(true);
   });
+
+  it("Todos los años divisibles por 100 pero no por 400 NO son años bisiestos", () => {
+    expect(bisiesto(1700)).toEqual(false);
+  });
 });
